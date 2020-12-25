c69a87b753b3d451603136537be8e7952e74bdb7
Niki Brown <nikibrown@Niki-Browns-MBP.attlocal.net>
2020-12-25 13:47:58 -0800

    add multiple queries to blog listing page

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import { RichText, Date } from 'prismic-reactjs'
 import { client, linkResolver, hrefResolver } from '../prismic-configuration'
 import Link from 'next/link'
 
-const BlogHome = ({ home, posts, counter }) => (
+
+const BlogHome = ({ home, posts, featuredPosts, counter }) => (
   <div>
 	  {/* <pre>{JSON.stringify({home})}</pre>
 	  <pre>{JSON.stringify({posts})}</pre> */}
@@ -12,16 +13,29 @@ const BlogHome = ({ home, posts, counter }) => (
     <h1>{RichText.asText(home.data.headline)}</h1>
 	<p>{home.data.test_field}</p>
     <p>{RichText.asText(home.data.description)}</p>
+
+	<h2>Featured Posts</h2>
+	<ul>
+	{featuredPosts.results.map((featuredPost) => (
+        <li key={featuredPost.uid}>
+			<Link href={hrefResolver(featuredPost)} as={linkResolver(featuredPost)} passHref>
+           		<a>{RichText.render(featuredPost.data.title)}</a>
+         	</Link>
+         <span>{Date(featuredPost.data.date).toString()}</span>
+		</li>
+      ))}
+	</ul>
+
+	<h2>Blog Posts</h2>
 	<ul>
 	
       {posts.results.map((post) => (
+		// insert gated_content something every three posts
         <li key={post.uid}>
 			<Link href={hrefResolver(post)} as={linkResolver(post)} passHref>
            		<a>{ counter++} {RichText.render(post.data.title)}</a>
          	</Link>
          <span>{Date(post.data.date).toString()}</span>
-
-			
 		</li>
       ))}
     </ul>
@@ -31,14 +45,25 @@ const BlogHome = ({ home, posts, counter }) => (
 export async function getServerSideProps({res}) {
 let counter = 1;
   const home = await client.getSingle('blog_home')
-  const posts = await client.query(
-    Prismic.Predicates.at('document.type', 'post'),
+  const featuredPosts = await client.query([
+	Prismic.Predicates.at('document.type', 'post'),
+	Prismic.Predicates.at("document.tags", ['Featured'])],
     { orderings: '[my.post.date desc]' }
   )
 
+  const posts = await client.query([
+	Prismic.Predicates.at('document.type', 'post'),
+	Prismic.Predicates.not("document.tags", ['Featured'])],
+    { orderings: '[my.post.date desc]' }
+  )
+
+  // use fullText for search https://prismic.io/docs/technologies/query-predicate-reference-javascript#fulltext
+
+  // insert gated_content something every three posts
+
   res.setHeader('Cache-Control', 's-maxage=1, stale-while-revalidate')
 
-  return { props: { home, posts, counter } }
+  return { props: { home, featuredPosts, posts, counter } }
 }
 
 export default BlogHome
