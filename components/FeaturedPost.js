1c92b94938b944ea792471b48feae436c3a8490e
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-02-11 16:10:55 -0800

    add FeaturedPost component

---
+++ components/FeaturedPost.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { RichText } from 'prismic-reactjs'
+import { default as NextLink } from 'next/link'
+import { linkResolver, hrefResolver } from '../prismic-configuration'
+import Image from 'next/image'
+
+const FeaturedPost = ({ featuredPost }) => {
+    return (
+        <div className="card h-100" key={featuredPost.uid}>
+            <NextLink
+                href={hrefResolver(featuredPost)}
+                as={linkResolver(featuredPost)}
+                passHref
+                shallow={true}>
+                <a>
+                    <Image
+                        src={featuredPost.featuredImage.url}
+                        alt={featuredPost.featuredImage.alt}
+                        className="card-img-top"
+                        width={featuredPost.featuredImage.dimensions.width}
+                        height={featuredPost.featuredImage.dimensions.height}
+                    />
+                    <div className="card-body">
+                        <h1 className="card-title">{RichText.asText(featuredPost.title)}</h1>
+                    </div>
+                </a>
+            </NextLink>
+        </div>
+    )
+}
+
+export default FeaturedPost
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import { RichText, Date } from 'prismic-reactjs'
 import { client, linkResolver, hrefResolver } from '../prismic-configuration'
 import { default as NextLink } from 'next/link'
 import Image from 'next/image'
+import FeaturedPost from '../components/FeaturedPost'
 
 const BlogHome = ({ home, allFeaturedPosts, allBlogContent }) => (
     <main>
@@ -32,29 +33,7 @@ const BlogHome = ({ home, allFeaturedPosts, allBlogContent }) => (
                 <div className="row">
                     {allFeaturedPosts.map((featuredPost, index) => (
                         <div className="col-lg-4 col-sm-12" key={index}>
-                            <div className="card h-100" key={featuredPost.uid}>
-                                <NextLink
-                                    href={hrefResolver(featuredPost)}
-                                    // Optional decorator for the path that will be shown in the browser URL bar
-                                    as={linkResolver(featuredPost)}
-                                    passHref
-                                    shallow={true}>
-                                    <a>
-                                        <Image
-                                            src={featuredPost.featuredImage.url}
-                                            alt={featuredPost.featuredImage.alt}
-                                            className="card-img-top"
-                                            width={featuredPost.featuredImage.dimensions.width}
-                                            height={featuredPost.featuredImage.dimensions.height}
-                                        />
-                                        <div className="card-body">
-                                            <h1 className="card-title">
-                                                {RichText.asText(featuredPost.title)}
-                                            </h1>
-                                        </div>
-                                    </a>
-                                </NextLink>
-                            </div>
+                            <FeaturedPost featuredPost={featuredPost} />
                         </div>
                     ))}
                 </div>
