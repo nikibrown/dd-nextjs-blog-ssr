e92fccbfdf31bfd4f3e1a085173d1910ade97827
Niki Brown <nikibrown@Niki-Browns-MacBook-Pro.local>
2021-02-19 10:56:49 -0800

    move search

--- pages/index.js
+++ pages/index.js
@@ -8,75 +8,6 @@ import BlogPost from '../components/BlogPost'
 
 const BlogHome = ({ home, allFeaturedPosts, allBlogContent }) => (
     <main>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container">
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    {/* <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">
-                                Home <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">
-                                Link
-                            </a>
-                        </li>
-                        <li className="nav-item dropdown">
-                            <a
-                                className="nav-link dropdown-toggle"
-                                href="#"
-                                id="navbarDropdown"
-                                role="button"
-                                data-toggle="dropdown"
-                                aria-haspopup="true"
-                                aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="#">
-                                    Action
-                                </a>
-                                <a className="dropdown-item" href="#">
-                                    Another action
-                                </a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="#">
-                                    Something else here
-                                </a>
-                            </div>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link disabled" href="#">
-                                Disabled
-                            </a>
-                        </li>
-                    </ul> */}
-                    <form className="form-inline my-2 my-lg-0">
-                        <input
-                            className="form-control mr-sm-2"
-                            type="search"
-                            placeholder="Search"
-                            aria-label="Search"
-                        />
-                        <button className="btn btn-primary my-2 my-sm-0" type="submit">
-                            Search
-                        </button>
-                    </form>
-                </div>
-            </div>
-        </nav>
         <div className="container">
             {/* IGNORE FOR TROUBLESHOOTING! <p>{JSON.stringify({ allBlogContent })}</p> 
 			<h1>featuredPosts (data from prismic)</h1>
@@ -90,11 +21,29 @@ const BlogHome = ({ home, allFeaturedPosts, allBlogContent }) => (
                     width={home.data.image.dimensions.width}
                     height={home.data.image.dimensions.height}
                     className="blog-image "
+                    preload="true"
                 />
                 <h1>{RichText.asText(home.data.headline)}</h1>
                 <p className="lead">{RichText.asText(home.data.description)}</p>
             </div>
         </div>
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <form className="form-inline my-2 my-lg-0">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        id="search"
+                        placeholder="Search our blog"
+                        aria-label="Search"
+                    />
+                    <button className="btn btn-primary my-2 my-sm-0" type="submit">
+                        🔍
+                    </button>
+                </form>
+            </div>
+        </nav>
 
         <section className="featured-posts jumbotron">
             <div className="container">
